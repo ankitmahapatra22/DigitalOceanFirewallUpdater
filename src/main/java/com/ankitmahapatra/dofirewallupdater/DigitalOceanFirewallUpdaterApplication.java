b0885b8b16a914bf1154212d86b003dd616ca63f
Ankit Mahapatra <ankitmahapatra22@gmail.com>
2020-10-31 23:13:13 +0530

    #9 - Added main method and print statements

--- src/main/java/com/ankitmahapatra/dofirewallupdater/DigitalOceanFirewallUpdaterApplication.java
+++ src/main/java/com/ankitmahapatra/dofirewallupdater/DigitalOceanFirewallUpdaterApplication.java
@@ -1,5 +1,10 @@
 package com.ankitmahapatra.dofirewallupdater;
 
 public class DigitalOceanFirewallUpdaterApplication {
-
+    
+    public static void main(String[] args){
+    	DigitalOceanFirewallUpdaterApplication digitalOceanFireWallUpdaterApplication =
+    			new DigitalOceanFirewallUpdaterApplication();
+    	System.out.println(" Starting with firewall update ...");
+    }
 }
