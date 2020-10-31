9dbeb4f3bc94283e4fc11bdd7deb5468dc7b9b19
Reghill <60101222+Reghill@users.noreply.github.com>
2020-11-01 01:27:06 +0530

    Add Method to fetch IP address

--- src/main/java/com/ankitmahapatra/dofirewallupdater/DigitalOceanFirewallUpdaterApplication.java
+++ src/main/java/com/ankitmahapatra/dofirewallupdater/DigitalOceanFirewallUpdaterApplication.java
@@ -7,4 +7,20 @@ public class DigitalOceanFirewallUpdaterApplication {
     			new DigitalOceanFirewallUpdaterApplication();
     	System.out.println(" Starting with firewall update ...");
     }
+    
+    private String getMyPublicIPAddress() throws UnknownHostException
+        {
+            String host = "https://api.ipify.org/";
+
+            try {
+                InetAddress ip = InetAddress.getByName(new URL(host)
+                        .getHost());
+
+               return String.valueOf(ip);
+            }
+            catch (MalformedURLException e) {
+                System.out.println("Invalid URL");
+            }
+            return "";
+        }
 }
